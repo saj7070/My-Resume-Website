6ecdc1262e59df46940e0091908c6716d473043b
ali <example@gmail.com>
2021-12-10 23:30:38 +0330

    upload files

---
+++ pic/myproject.js
@@ -0,0 +1,8 @@
+function payam() {
+  var val = confirm("آیا از ارسال فرم اطمینان دارید ؟");
+  if (val == true) {
+      alert("عملیات با موفقیت انجام شد !");
+  } else {
+      alert("ارسال فرم لغو شد");
+  }
+}
